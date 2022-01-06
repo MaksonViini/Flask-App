e6b159a622b9e597a118fa8407d64934ef174ad1
maksonviini <maksonvinicio7@gmail.com>
2022-01-06 13:52:45 -0300

    Adicionando deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo cu
-# sudo apt-get install -y python3-pip
-# sudo pip3 install --upgrade setuptools
-# sudo pip3 install docker-compose
-# sudo docker-compose up -d
+
+sudo apt-get install -y python3-pip
+sudo pip3 install --upgrade setuptools
+sudo pip3 install docker-compose
+sudo docker-compose up -d
