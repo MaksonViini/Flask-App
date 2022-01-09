59437e6076ea45bdb68103f8304ce6d170f1d693
maksonviini <maksonvinicio7@gmail.com>
2022-01-09 19:23:40 -0300

    Adicionando os arquivos ao github novamente, nao ao gitlab

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-sudo apt-get install -y python3-pip
-sudo pip3 install --upgrade setuptools
-sudo pip3 install docker-compose
-sudo docker-compose up -d
+echo Executei
+
+# sudo su
+# sudo apt-get install -y python3-pip
+# sudo pip3 install --upgrade setuptools
+# sudo pip3 install docker-compose
+# sudo docker-compose up -d
