50fe935f5f06f2a82ef14f65a028e38edd7d9f88
maksonviini <maksonvinicio7@gmail.com>
2022-01-22 17:53:43 -0300

    inicializando

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-echo Executei
-
-# sudo su
-# sudo apt-get install -y python3-pip
-# sudo pip3 install --upgrade setuptools
-# sudo pip3 install docker-compose
-# sudo docker-compose up -d
+sudo su
+sudo apt-get install -y python3-pip
+sudo pip3 install --upgrade setuptools
+sudo pip3 install docker-compose
+sudo docker-compose up -d
